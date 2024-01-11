ba7f08ddb7a86044c09504e0cee84a77f0272616
SowmyaReddy513 <ksowmyareddy513@gmail.com>
2024-01-10 22:02:29 -0600

    added Student class

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    private String name = "Sowmya";
+
+    public Student(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name();
+    }
+}
