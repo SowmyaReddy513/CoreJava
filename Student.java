a58cc17d79bc6a530a546e4f4965a83370ce7d4d
SowmyaReddy513 <ksowmyareddy513@gmail.com>
2024-01-11 13:54:48 -0600

    Made changes to student.java file

--- Student.java
+++ Student.java
@@ -1,11 +1,20 @@
 public class Student {
-
-    private String name = "Sowmya";
+    private String name;
 
     public Student(String name) {
         this.name = name;
     }
-    public String getName(){
-        return name();
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+        Student student = new Student("Sowmya Reddy");
+
+        //getName method to retrieve the name
+        String studentName = student.getName();
+        System.out.println(studentName);
     }
 }
